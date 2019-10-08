3f08963b9bbd75a1fb3c8f1e5b8856aafc314b27
glglthssla@naver.com <GonG0731!>
2019-10-08 21:55:48 +0900

    modify RegistActivity (동일한 비밀번호인지 확인 - 같은 경우 ck.png/ 다른 경우 nck.png) modify ReservationStatusActivity (Content-Type json으로 변경) add CarKeyActivity (원격키 사용을 위한 버튼 생성 예정)

---
+++ app/src/main/java/com/example/myapplication/CarKeyActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarKeyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_key);
+    }
+}
